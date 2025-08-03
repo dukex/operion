db84aa15001e3d6ecc3d36f40e1671b4686d4d49
Duke <emersonalmeidax@gmail.com>
2025-08-03 11:17:52 -0300

    fix lint

--- pkg/models/workflow_step.go
+++ pkg/models/workflow_step.go
@@ -4,11 +4,11 @@ type StepType string
 
 type WorkflowStep struct {
 	ID            string         `json:"id"`
-	ActionID      string         `json:"action_id" validate:"required"`
+	ActionID      string         `json:"action_id"            validate:"required"`
 	Configuration map[string]any `json:"configuration"`
-	UID           string         `json:"uid" validate:"required,lowercase,alphanum"`
-	Name          string         `json:"name" validate:"required"`
+	UID           string         `json:"uid"                  validate:"required,lowercase,alphanum"`
+	Name          string         `json:"name"                 validate:"required"`
 	OnSuccess     *string        `json:"on_success,omitempty"`
 	OnFailure     *string        `json:"on_failure,omitempty"`
-	Enabled       bool           `json:"enabled" validate:"required"`
+	Enabled       bool           `json:"enabled"              validate:"required"`
 }
